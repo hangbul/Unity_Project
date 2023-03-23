096bebcbfe6db233b3c13d1f5675bc673c7a523e
DESKTOP-D3O6O7N\Atents <wns0685@gmail.com>
2023-03-23 15:22:31 +0900

    delegate 및 unityAction, unityEvent

---
+++ Assets/Data/Character_Test/Script/PlayerProperty.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperty : MonoBehaviour
+{
+    public float MoveSpeed = 1.0f;
+    public float RotSpeed = 360.0f;
+
+    Animator _anim = null;
+
+    protected Animator MyAnim
+    {
+        get
+        {
+            _anim = GetComponent<Animator>();
+            if(_anim == null)
+            {
+                _anim = GetComponentInChildren<Animator>();
+            }
+            return _anim;
+        }
+
+    }
+
+}
