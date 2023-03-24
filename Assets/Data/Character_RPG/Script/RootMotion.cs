cef2443199e298d16aa58587382189c03440d36c
DESKTOP-D3O6O7N\Atents <wns0685@gmail.com>
2023-03-24 11:58:12 +0900

    Root모션 중 문제 해결 1. Model / Animation / Root  Transform Rotation 회전 무시 2. Model / Animation / Root  Transform Position(Y) Y값 이동 무시 3. Model / Animation / Root  Transform Position(XZ) XZ평면 이동 무시
    Upon 값 (기준선 : 기본적으로 Original)

---
+++ Assets/Data/Character_RPG/Script/RootMotion.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootMotion : MonoBehaviour
+{
+    public Transform Root;
+
+    void Start()
+    {
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        
+    }
+}
