96b70a6a460d31121a2027b69bd691f645338c50
DESKTOP-D3O6O7N\Atents <wns0685@gmail.com>
2023-05-12 10:03:41 +0900

    파이프 라인 : 각 단계의 쉐이더 정보( 정점 쉐이더, 기하 도형 쉐이더, 픽셀 쉐이더) 레스터라이저 단계 (픽셀화, 2d 형태 전환) 정점/ 기하 도형 단계에서는 가상 공간에 위치하며, 레스터라이저 단계를 거쳐서 시각적 공간으로 전환된다.

--- Assets/Resources/Tank/Tank_Control_Picking.cs
+++ Assets/Resources/Tank/Tank_Control_Picking.cs
@@ -70,9 +70,9 @@ public class Tank_Control_Picking : MonoBehaviour
             _bomb = null;
             //GameObject obj = Instantiate(orgBomb, _point[count]);
             //GameObject obj = ObjectPool<Bomb>.Inst.GetObject(orgBomb, _point[count].position, _point[count].rotation, _point[count]);
-            obj.transform.localPosition = Vector3.zero;
-            obj.transform.localRotation = _point[count].localRotation; ;
-            _bomb = obj.GetComponent<Bomb>();
+            //obj.transform.localPosition = Vector3.zero;
+            //obj.transform.localRotation = _point[count].localRotation; ;
+            //_bomb = obj.GetComponent<Bomb>();
             StartCoroutine(EffectPlay());
         }
 
